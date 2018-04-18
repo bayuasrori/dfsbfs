cde40083720fc0beb26791a62bfb8853bde01a43
ninjalame <bayu.asrori@gmail.com>
2018-04-18 09:22:52 +0700

    menambahkan algortima bfs

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 const main = require('../controllers/main');
 const dfs = require('../controllers/dfs');
+const bfs = require('../controllers/bfs');
 /* GET home page. */
 router.get('/', main.index);
 router.post('/dfs', dfs.reqDFS);
+router.post('/bfs', bfs.reqBFS);
 module.exports = router;
